5519af37c59014037ea7b1ce58397f8e29e4e2c8
Henrique <riquessan@gmail.com>
2021-10-08 09:51:47 -0300

    Hello World Changes

--- App.tsx
+++ App.tsx
@@ -1,28 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {Text, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+import { Text } from 'react-native';
 
-  return (
-    <Text
-      style={{
-        backgroundColor: isDarkMode ? Colors.black : Colors.white,
-      }}>
-      Hello World!
-    </Text>
-  );
+export const App = (): JSX.Element => {
+  return <Text>Hello World!</Text>;
 };
-
-export default App;

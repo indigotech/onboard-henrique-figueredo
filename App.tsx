28f8efee967794c51320c956ef4e0549b3f367d6
Henrique <riquessan@gmail.com>
2021-10-08 19:11:03 -0300

    Styled Components Theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 
-const App = (): JSX.Element => {
-  return <Text>Hello World!</Text>;
+import { ScreenLogin } from './src/screens/login';
+import { light } from './src/themes/default';
+
+export const App = (): JSX.Element => {
+  return (
+    <ThemeProvider theme={light}>
+      <ScreenLogin />
+    </ThemeProvider>
+  );
 };
-
 export default App;

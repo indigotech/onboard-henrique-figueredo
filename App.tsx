183be84320f8443f82d07ba5d8446857fed62ce9
Henrique <riquessan@gmail.com>
2021-10-09 12:09:43 -0300

    Reafactor Paths

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
 
-import { ScreenLogin } from './src/screens/login';
+import { ScreenLogin } from './src/app/modules/login/login';
 import { light } from './src/themes/default';
 
 export const App = (): JSX.Element => {

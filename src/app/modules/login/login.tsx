83b6540a37c3aec6e0f1de4b84ba91523c2d6074
Henrique <riquessan@gmail.com>
2021-10-09 12:09:43 -0300

    Reafactor Paths

---
+++ src/app/modules/login/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { PageLogin } from '../../../atomic/pag.login/login.component';
+
+export const ScreenLogin: React.FC = () => {
+  return <PageLogin />;
+};
--- src/atomic/atm.input/input.component.style.ts
+++ src/atomic/atm.input/input.component.style.ts
@@ -4,5 +4,5 @@ export const StyleInput = styled.TextInput`
   border: ${(props) => props.theme.colors.grey[600]};
   border-radius: ${(props) => props.theme.border.sm};
   padding: ${(props) => props.theme.spacing.sm};
-  font-size: 22px;
+  font-size: ${(props) => props.theme.font.size.h1};
 `;

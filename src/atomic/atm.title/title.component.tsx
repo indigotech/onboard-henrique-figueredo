f1f66c198e099947b945039df0e1b7694fffc5cc
Henrique <riquessan@gmail.com>
2021-10-10 12:16:12 -0300

    Atom Title

---
+++ src/atomic/atm.title/title.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { StyleTitle } from './title.component.style';
+
+export const AtomTitle: React.FC = ({ children }) => {
+  return <StyleTitle>{children}</StyleTitle>;
+};

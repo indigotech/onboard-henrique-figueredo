ae4bcd9dd3f564eda6c847e3cd2dd1fb529f9b0b
Henrique <riquessan@gmail.com>
2021-10-11 12:34:49 -0300

    Input Props and new replace AtomSubText to AtomLabel

--- src/atomic/mol.named-input/named-input.component.tsx
+++ src/atomic/mol.named-input/named-input.component.tsx
@@ -3,18 +3,24 @@ import React from 'react';
 import { useTheme } from 'styled-components';
 
 import { AtomInput } from '../atm.input/input.component';
+import { AtomLabel } from '../atm.label/label.component';
 import { AtomSeparator } from '../atm.separator/separator.component';
-import { AtomSubText } from '../atm.subtext/subtext.component';
 
 import { MoleculeNamedInputContainer } from './named-input.component.style';
 
-export const MoleculeNamedInput: React.FC = ({ children }) => {
+interface Props {
+  value: string;
+  onInputChange(value: string): void;
+  secureTextEntry?: boolean;
+}
+
+export const MoleculeNamedInput: React.FC<Props> = ({ children, value, onInputChange, secureTextEntry }) => {
   const { spacing } = useTheme();
   return (
     <MoleculeNamedInputContainer>
-      <AtomSubText>{children}</AtomSubText>
+      <AtomLabel color="secondary">{children}</AtomLabel>
       <AtomSeparator size={spacing.sm} />
-      <AtomInput />
+      <AtomInput value={value} onInputChange={onInputChange} secureTextEntry={secureTextEntry} />
     </MoleculeNamedInputContainer>
   );
 };

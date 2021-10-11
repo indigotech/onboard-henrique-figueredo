306d5ddd9685a1ec463672669b7f0182e74ee9b5
Henrique <riquessan@gmail.com>
2021-10-11 12:30:53 -0300

    Input receives logic

--- src/atomic/atm.input/input.component.tsx
+++ src/atomic/atm.input/input.component.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { StyleInput } from './input.component.style';
 
-export const AtomInput: React.FC = () => {
-  return <StyleInput />;
+interface Props {
+  value: string;
+  onInputChange(value: string): void;
+  secureTextEntry?: boolean;
+}
+export const AtomInput: React.FC<Props> = ({ value, onInputChange, secureTextEntry }) => {
+  return <StyleInput value={value} onChangeText={onInputChange} secureTextEntry={secureTextEntry} />;
 };

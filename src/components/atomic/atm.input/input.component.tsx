ded7d42ff2e82347a0b857398a956ebb0a3c2c62
Henrique <riquessan@gmail.com>
2021-10-13 15:46:49 -0300

    Reafactor Paths

--- src/components/atomic/atm.input/input.component.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { StyleInput } from './input.component.style';
-
-export const AtomInput: React.FC = () => {
-  return <StyleInput />;
-};
--- src/components/atomic/atm.subtext/subtext.component.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { StyleText } from './subtext.component.style';
-
-export const AtomSubText: React.FC = ({ children }) => {
-  return <StyleText>{children}</StyleText>;
-};

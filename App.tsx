dfb786c240ef0792b4ce105e7d0c6c984884754e
Henrique <riquessan@gmail.com>
2021-10-14 08:21:48 -0300

    Setup Apollo Client

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 
 import { ScreenLogin } from './src/app/modules/login/login';
+import { client } from './src/configs/apolloclient';
 import { light } from './src/themes/default';
 
 export const App = (): JSX.Element => {
   return (
-    <ThemeProvider theme={light}>
-      <ScreenLogin />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={light}>
+        <ScreenLogin />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 export default App;

e151efcd7ea06b4ef519506675ba9c30f6ba41ff
Henrique <riquessan@gmail.com>
2021-10-14 08:23:02 -0300

    Change Apollo Client file name

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 
 import { ScreenLogin } from './src/app/modules/login/login';
-import { client } from './src/configs/apolloclient';
+import { client } from './src/configs/apollo-client';
 import { light } from './src/themes/default';
 
 export const App = (): JSX.Element => {

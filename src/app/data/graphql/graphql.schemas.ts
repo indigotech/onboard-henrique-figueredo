07278d3b4fb686a0b12d51e9a0c68fc847172271
Henrique <riquessan@gmail.com>
2021-10-18 11:46:01 -0300

    Create get users Query

--- src/app/data/graphql/graphql.schemas.ts
+++ src/app/data/graphql/graphql.schemas.ts
@@ -1,7 +1,12 @@
 /// <reference path="../../declarations.ts" />
 
 import Login from './mutation/login.mutation.graphql';
+import GetUsers from './query/get-users.query.graphql';
 
 export const Mutation = {
   Login,
+};
+
+export const Query = {
+  GetUsers,
 };

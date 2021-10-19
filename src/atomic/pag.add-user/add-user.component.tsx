692ecfb494ff8bbff6d0a194b182bf7baf933a60
Henrique <riquessan@gmail.com>
2021-10-19 19:41:12 -0300

    Page Wrapper in Add User

--- src/atomic/pag.add-user/add-user.component.tsx
+++ src/atomic/pag.add-user/add-user.component.tsx
@@ -4,6 +4,7 @@ import { Picker } from '@react-native-picker/picker';
 import { ScrollView } from 'react-native';
 import DatePicker from 'react-native-date-picker';
 
+import { PageWrapper } from '../../themes/global';
 import { AtomLabel } from '../atm.label/label.component';
 import { AtomSeparator } from '../atm.separator/separator.component';
 import { AtomTitle } from '../atm.title/title.component';
@@ -49,49 +50,51 @@ export const PageAddUser: React.FC<Props> = ({
 
   return (
     <ScrollView>
-      <AtomSeparator size={'xlg'} />
-      <AtomTitle title="Adicionar usuário" />
+      <PageWrapper>
+        <AtomSeparator size={'xlg'} />
+        <AtomTitle title="Adicionar usuário" />
 
-      <AtomSeparator size={'md'} />
-      <AtomLabel text={message.text} color={message.error ? 'error' : 'callToAction'} />
+        <AtomSeparator size={'md'} />
+        <AtomLabel text={message.text} color={message.error ? 'error' : 'callToAction'} />
 
-      <AtomSeparator size={'md'} />
-      <MoleculeNamedInput text="Nome" value={name} onInputChange={changeName} />
+        <AtomSeparator size={'md'} />
+        <MoleculeNamedInput text="Nome" value={name} onInputChange={changeName} />
 
-      <AtomSeparator size={'xlg'} />
-      <MoleculeNamedInput text="Email" value={email} onInputChange={changeEmail} />
+        <AtomSeparator size={'xlg'} />
+        <MoleculeNamedInput text="Email" value={email} onInputChange={changeEmail} />
 
-      <AtomSeparator size={'xlg'} />
-      <MoleculeNamedInput text="Telefone" value={phone} onInputChange={changePhone} />
+        <AtomSeparator size={'xlg'} />
+        <MoleculeNamedInput text="Telefone" value={phone} onInputChange={changePhone} />
 
-      <AtomSeparator size={'xlg'} />
-      <MoleculeButton title="Data de nascimento" color="primary" onPress={() => setIsDateOpen(true)} />
-      <DatePicker
-        modal
-        mode="date"
-        locale="pt-Br"
-        is24hourSource="locale"
-        open={isDateOpen}
-        date={birthDate}
-        maximumDate={new Date()}
-        onDateChange={changeBirthDate}
-        onConfirm={() => {
-          setIsDateOpen(false);
-        }}
-        onCancel={() => setIsDateOpen(false)}
-      />
+        <AtomSeparator size={'xlg'} />
+        <MoleculeButton title="Data de nascimento" color="primary" onPress={() => setIsDateOpen(true)} />
+        <DatePicker
+          modal
+          mode="date"
+          locale="pt-Br"
+          is24hourSource="locale"
+          open={isDateOpen}
+          date={birthDate}
+          maximumDate={new Date()}
+          onDateChange={changeBirthDate}
+          onConfirm={() => {
+            setIsDateOpen(false);
+          }}
+          onCancel={() => setIsDateOpen(false)}
+        />
 
-      <AtomSeparator size={'xlg'} />
-      <MoleculeNamedInput text="Password" secureTextEntry value={password} onInputChange={changePassword} />
+        <AtomSeparator size={'xlg'} />
+        <MoleculeNamedInput text="Password" secureTextEntry value={password} onInputChange={changePassword} />
 
-      <AtomSeparator size={'xlg'} />
-      <Picker selectedValue={role} onValueChange={changeRole}>
-        <Picker.Item label="User" value={'user'} />
-        <Picker.Item label="Admin" value={'admin'} />
-      </Picker>
+        <AtomSeparator size={'xlg'} />
+        <Picker selectedValue={role} onValueChange={changeRole}>
+          <Picker.Item label="User" value={'user'} />
+          <Picker.Item label="Admin" value={'admin'} />
+        </Picker>
 
-      <AtomSeparator size={'xlg'} />
-      <MoleculeButton loading={loading} title="Adicionar" color="callToAction" onPress={submitUser} />
+        <AtomSeparator size={'xlg'} />
+        <MoleculeButton loading={loading} title="Adicionar" color="callToAction" onPress={submitUser} />
+      </PageWrapper>
     </ScrollView>
   );
 };

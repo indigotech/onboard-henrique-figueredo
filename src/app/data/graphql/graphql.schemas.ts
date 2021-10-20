706ee086c385ac8d7d1951c3581cf90466a933f3
Henrique <riquessan@gmail.com>
2021-10-20 09:18:46 -0300

    Add User Mutation

--- src/app/data/graphql/graphql.schemas.ts
+++ src/app/data/graphql/graphql.schemas.ts
@@ -1,10 +1,12 @@
 /// <reference path="../../declarations.ts" />
 
+import AddUser from './mutation/add-user.mutation.graphql';
 import Login from './mutation/login.mutation.graphql';
 import GetUsers from './query/get-users.query.graphql';
 
 export const Mutation = {
   Login,
+  AddUser,
 };
 
 export const Query = {
